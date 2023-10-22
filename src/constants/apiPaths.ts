560c526bf4b9c4bbf03ce301c95ba28c20743627
Nurlan K <nurlan.wap3@gmail.com>
2023-10-22 18:13:54 +0300

    feat: task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,12 @@
 const API_PATHS = {
   get api() {
-    return 'https://kvk5koj7u5.execute-api.eu-west-1.amazonaws.com/dev';
+    return "https://kvk5koj7u5.execute-api.eu-west-1.amazonaws.com/dev";
   },
   get products() {
     return `${this.api}/products`;
   },
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://vgcgj91pw6.execute-api.eu-west-1.amazonaws.com/dev/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
